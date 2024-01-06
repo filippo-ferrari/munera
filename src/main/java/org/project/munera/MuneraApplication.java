67ab6e7796e28261ed85dbaeac69586b111160d3
filippo-ferrari <filippo.ferrari.pr@gmail.com>
2024-01-06 16:50:03 +0100

    initial commit

---
+++ src/main/java/org/project/munera/MuneraApplication.java
@@ -0,0 +1,13 @@
+package org.project.munera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuneraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuneraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/munera/MuneraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.munera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuneraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
