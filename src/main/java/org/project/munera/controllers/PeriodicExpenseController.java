298a3e88ca7116d4dc0928b01e799ce394e18f1f
filippo-ferrari <filippo.ferrari.pr@gmail.com>
2024-01-15 20:00:05 +0100

    PeriodicExpenseController created

--- src/main/java/org/project/munera/controllers/PeriodicExpenseController.java
+++ src/main/java/org/project/munera/controllers/PeriodicExpenseController.java
@@ -1,8 +1,9 @@
 package org.project.munera.controllers;
 
+import org.project.munera.entities.PeriodicExpense;
 import org.project.munera.services.PeriodicExpenseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class PeriodicExpenseController {
@@ -11,5 +12,31 @@ public class PeriodicExpenseController {
 
     public PeriodicExpenseController(PeriodicExpenseService expenseService){
         this.expenseService = expenseService;
+    }
+
+
+    @GetMapping("/periodicExpenses")
+    public List<PeriodicExpense> list() {
+        return expenseService.fetchPeriodicExpenses();
+    }
+
+    @GetMapping("/periodicExpenses/{periodicExpense}")
+    public PeriodicExpense reference(@PathVariable PeriodicExpense periodicExpense) {
+        return periodicExpense;
+    }
+
+    @PostMapping("/periodicExpenses")
+    public PeriodicExpense create(@RequestBody PeriodicExpense periodicExpense) {
+        return this.expenseService.addNewPeriodicExpense(periodicExpense);
+    }
+
+    @PatchMapping("/periodicExpenses/{periodicExpense}")
+    public PeriodicExpense patch(@PathVariable("periodExpense") PeriodicExpense expenseToPatch, @RequestBody PeriodicExpense expensePatched) {
+        return this.expenseService.patchExpense(expenseToPatch, expensePatched);
+    }
+
+    @DeleteMapping("/periodicExpenses/{periodicExpense}")
+    public void delete(@PathVariable PeriodicExpense periodicExpense) {
+        this.expenseService.deletePeriodicExpense(periodicExpense);
     }
 }
