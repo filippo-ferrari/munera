4b503323df3b7c68a3a25e225d6b5820957c41ef
start.vaadin.com <no-reply@vaadin.com>
2024-05-18 14:42:12 +0000

    Generated project

---
+++ src/main/java/com/application/munera/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.application.munera.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/application/munera/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.application.munera.services;
+
+import org.springframework.lang.NonNullApi;
