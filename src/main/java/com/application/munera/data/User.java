bdcff2461c76b6f7c0aa5106cd70258f58ff1ccd
filippo-ferrari <filippo.ferrari.pr@gmail.com>
2024-09-06 19:14:26 +0200

    feat: created user entity

---
+++ src/main/java/com/application/munera/data/User.java
@@ -0,0 +1,41 @@
+package com.application.munera.data;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id", unique = true, nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @Column(name = "FirstName", nullable = false)
+    private String firstName;
+
+    @Size(max = 100)
+    @Column(name = "LastName", nullable = false)
+    private String lastName;
+
+    @Size(max = 100)
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Size(max = 100)
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "roles", nullable = false)
+    private String roles;
+
+    @Email
+    @Size(max = 100)
+    @Column(name = "email")
+    private String email;
+}
